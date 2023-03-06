ac05f755a506624ddfce6a559fca77a8ed1014a2
Remi DIJOUX <remidijoux@gmail.com>
2023-03-06 11:19:21 +0100

    Create user

--- site/createUser.php
+++ site/createUser.php
@@ -8,17 +8,27 @@
 </head>
 <body>
     <form action=".php" method="POST">
-        <input type="text" placeholder="Nom" name="nom" required> 
-        <br><br>       
+        <input type="text" placeholder="Nom" name="nom" required>
+        <br><br>
         <input type="text" placeholder="Prenom" name="prenom" required>
-        <br><br>        
+        <br><br>
         <input type="text" placeholder="email" name="email" required>
-        <br><br>        
+        <br><br>
         <input type="text" placeholder="role" name="username" required>
         <br><br>
         <input type="password" placeholder="Mot de passe" name="password" required>
         <br><br><br>
         <input type="submit" id='submit' value='Créer' >
       </form>
+
+
+      <?php
+      require_once('../test/class.php');
+
+      if(isset($_POST['submit'])){
+      creer_user($_GET['nom'], $_GET['prenom'], $_GET['email'], $_GET['username'],$_GET['password'] );
+      }
+       ?>
+
 </body>
 </html>
